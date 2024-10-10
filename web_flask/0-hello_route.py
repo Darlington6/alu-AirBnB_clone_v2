154e2a9790754ddc7cf8b83b0402903e3ea0f1c1
carine-ahishakiye <c.yibukabay@alustudent.com>
2024-10-10 14:05:57 +0200

    Hello Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -13,9 +13,4 @@ def hello_hbnb():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-<<<<<<< HEAD
     app.url_map.strict_slashes = False
-=======
-    app.url_map.strict_slashes = False
-
->>>>>>> 9bcb7f831f881b3aa5220fd8fc2e494f27320f6e
